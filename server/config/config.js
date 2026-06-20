4875cf2609ea763b7a19d8c717446f19ec96d97d
prashant272 <prashantkrjha12@gmail.com>
2026-06-20 17:14:41 +0530

    add docker and docker ingnore file

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const config = {
-    PORT: process.env.PORT || 5001,
+    PORT: process.env.PORT || 5000,
     MONGO_URI: process.env.MONGO_URI,
     MONGO_DB_NAME: process.env.MONGO_DB_NAME,
     JWT_SECRET: process.env.JWT_SECRET,
