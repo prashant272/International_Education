0cd632936b72281c7b981156caa2f2cf877064db
prashant272 <prashantkrjha12@gmail.com>
2026-06-21 16:27:11 +0530

    replace image features

--- client/src/constants/home/events.js
+++ client/src/constants/home/events.js
@@ -1,12 +1,11 @@
 export const events = [
     {
-        title: "International Education Award, 2026 –Mumbai Edition",
-        date: "12 July,2026",
+        title: "10th International Education Awards & Conference",
+        date: "8th August 2026",
         desc: "Honouring the visionary educators and modern institutions rewriting the future of learning and guiding the next generation of changemakers.",
         place: "Mumbai, India",
         highlight: "National Excellence",
-        banner: "../indiabrand-icon-award.jpeg",
-        slug: "https://www.indiabrandicon.in/upcoming-editions/2026/india-brand-icon-awards-conference"
-
+        banner: "../10th-international-education-awards.jpg",
+        slug: ""
     },
 ];
